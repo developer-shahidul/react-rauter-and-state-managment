35fc9117467576ea4de312c5545abf7fe162447f
developer-shahidul <developer-shahidul@outlook.com>
2025-09-15 21:36:54 +0600

    create nav toggle and navbar animation

--- price-options/src/components/navbar/Navbar.jsx
+++ price-options/src/components/navbar/Navbar.jsx
@@ -1,8 +1,12 @@
 //
 
+import { useState } from "react";
 import Link from "../link/Link";
+import { HiOutlineMenuAlt1 } from "react-icons/hi";
+import { RxCross2 } from "react-icons/rx";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   const routes = [
     { id: 1, name: "Home", path: "/" },
     { id: 2, name: "About", path: "/about" },
@@ -13,6 +17,9 @@ const Navbar = () => {
 
   return (
     <nav>
+      <div className="md:hidden text-3xl" onClick={() => setOpen(!open)}>
+        {open ? <RxCross2></RxCross2> : <HiOutlineMenuAlt1></HiOutlineMenuAlt1>}
+      </div>
       <ul className="md:flex">
         {routes.map((route) => (
           <Link key={route.id} route={route}></Link>
