8688d67389dd06b5c49bf07e70fc58cc633fa781
developer-shahidul <developer-shahidul@outlook.com>
2025-09-17 01:12:29 +0600

    react awsome component use

--- price-options/src/components/phones/Phone.jsx
+++ price-options/src/components/phones/Phone.jsx
@@ -3,9 +3,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { BarChart, Bar, XAxis, YAxis, Tooltip, LabelList } from "recharts";
+import { Audio } from "react-loader-spinner";
 
 const Phone = () => {
   const [phones, setPhones] = useState([]);
+  const [loading, setLoading] = useState(true);
   //   console.log(phones);
   // broken phone repository theke neya Api, progrming hero git theke neya
   useEffect(() => {
@@ -25,11 +27,27 @@ const Phone = () => {
           return obj;
         });
         setPhones(phoesWithFakeData);
+        setLoading(false);
       }
     );
   }, []);
   return (
     <div>
+      {/* spinning */}
+      {loading && (
+        <div>
+          <Audio
+            height="80"
+            width="80"
+            radius="9"
+            color="green"
+            ariaLabel="three-dots-loading"
+            wrapperStyle
+            wrapperClass
+          />
+        </div>
+      )}
+
       <h2 className="text-5xl">Phones : {phones.length}</h2>
       <BarChart width={1200} height={400} data={phones}>
         <Bar dataKey="price" fill="#8884d8">
