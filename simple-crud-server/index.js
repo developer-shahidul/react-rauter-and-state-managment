3612052dbc7c8d756546f7ee72d208bb6867b92f
developer-shahidul <developer-shahidul@outlook.com>
2025-11-16 17:00:41 +0600

    create mongodb atlas account for simple crud server

---
+++ simple-crud-server/index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 5000;
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// ssshahidulislam71_db_user
+// /0QGMyMdAJT4CEZrW
+
+const uri =
+  "mongodb+srv://ssshahidulislam71_db_user:0QGMyMdAJT4CEZrW@cluster0.39yqdr4.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("SIMPLE CRUD IS RUNNING");
+});
+// applicaton chalanur jonno listen chalabo
+app.listen(port, () => {
+  console.log(`simple crud running on Port : ${port}`);
+});
+
+//1
+// try {
+// } catch (error) {
+// } finally {
+// }
+
+// 2
+// async function run(params) {}
+
+// run().catch((Error) => console.log(Error));
