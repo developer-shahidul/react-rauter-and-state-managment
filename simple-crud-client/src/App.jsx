76a2f06658584411eff14dc4e009af47c50a6e6d
developer-shahidul <developer-shahidul@outlook.com>
2025-11-19 23:53:46 +0600

    delete a User from the database using UI click button

--- simple-crud-client/src/App.jsx
+++ simple-crud-client/src/App.jsx
@@ -1,32 +1,29 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [user, setUser] = useState([]);
   const handleAddUser = (event) => {
     event.preventDefault();
     const form = event.target;
     const name = form.name.value;
     const email = form.email.value;
-    const newUser = { name, email };
-    console.log(newUser);
+    const user = { name, email };
+    console.log(user);
 
     fetch("http://localhost:5000/users", {
       method: "post",
       headers: {
         "content-type": "application/json",
       },
-      body: JSON.stringify(newUser),
+      body: JSON.stringify(user),
     })
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
+
         if (data.insertedId) {
           alert("users added successfully");
           form.reset();
         }
-        const newUser = [...user, data];
-        setUser(newUser);
       });
   };
 
@@ -40,13 +37,6 @@ function App() {
         <br /> <br />
         <input type="submit" name="add user" id="" />
       </form>
-      <div>
-        {user.map((p, index) => (
-          <p key={`${p.id}-${index}`}>
-            {p.name} : {p.email}
-          </p>
-        ))}
-      </div>
     </>
   );
 }
--- simple-crud-client/src/component/Users.jsx
+++ simple-crud-client/src/component/Users.jsx
@@ -3,13 +3,29 @@ import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
   const user = useLoaderData();
+
+  const handleDeletUser = (_id) => {
+    console.log("delet", _id);
+    fetch(`http://localhost:5000/users/${_id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.deletedCount > 0) {
+          alert("delete successfully");
+        }
+      });
+  };
+
   return (
     <div>
       <h2>{user.length}</h2>
       <div>
-        {user.map((user) => (
-          <p key={user._id}>
-            {user.name} : {user.email}
+        {user.map((q, index) => (
+          <p key={`${q._id}-${index}`}>
+            {q.name} : {q.email}
+            <button onClick={() => handleDeletUser(q._id)}>x</button>
           </p>
         ))}
       </div>
