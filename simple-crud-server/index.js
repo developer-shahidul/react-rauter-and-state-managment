f5b00f8442ece4e51d8c7a026278884454a6cd9b
developer-shahidul <developer-shahidul@outlook.com>
2025-11-19 21:13:22 +0600

    read data find multiple user data and display on UI

--- simple-crud-server/index.js
+++ simple-crud-server/index.js
@@ -31,6 +31,12 @@ async function run() {
     const database = client.db("userDB");
     const usersCollection = database.collection("users");
 
+    app.get("/users", async (req, res) => {
+      const cursor = usersCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       const result = await usersCollection.insertOne(user);
