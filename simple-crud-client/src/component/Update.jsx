a2f5598e57186aa0a07f1446ea42f143a3185abd
developer-shahidul <developer-shahidul@outlook.com>
2025-11-25 23:50:54 +0600

    explore put and patch and update user to database

--- simple-crud-client/src/component/Update.jsx
+++ simple-crud-client/src/component/Update.jsx
@@ -1,12 +1,10 @@
 //
 
-// import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Update = () => {
   const usersLoader = useLoaderData();
   console.log(usersLoader);
-  // const [user, setUser] = useState(usersLoader);
 
   const handleUpdateUser = (event) => {
     event.preventDefault();
@@ -14,7 +12,22 @@ const Update = () => {
     const name = form.name.value;
     const email = form.email.value;
     console.log(name, email);
+    const updatedUser = { name, email };
+
+    fetch(`http://localhost:5000/users/${usersLoader._id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(updatedUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount > 0) {
+          alert("User updated successfully");
+        }
+      });
   };
+
   return (
     <div>
       {<h3>Updated information of {usersLoader.name}</h3>}
